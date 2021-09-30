703662a01f6725804660773c95ac08bd7d153352
franjpr <franpr90@gmail.com>
2021-09-30 23:49:15 +0100

    feat: modelos para el boe

---
+++ src/services/models/boe.model.ts
@@ -0,0 +1,43 @@
+interface UrlPdf {
+  _: string;
+}
+
+export interface Item {
+  id: string;
+  titulo: string;
+  urlpdf: UrlPdf;
+}
+
+export interface Departamento {
+  nombre: string;
+  item: Item[];
+}
+
+export interface Seccion {
+  num: string;
+  nombre: string;
+  departamento: Departamento[];
+}
+
+export interface Diario {
+  nbo: string;
+  sumario_nbo: string;
+  seccion: Seccion[];
+}
+
+export interface Meta {
+  pub: string;
+  anno: string;
+  fecha: string;
+  fechainv: string;
+  fechaant: string;
+  fechaantant: string;
+  fechasig: string;
+  fechapub: string;
+  pubdate: string;
+}
+
+export interface Sumarioo {
+  diario: Diario;
+  meta: Meta;
+}
