7947bb425822e01194ff1c2814467a25dbfdff0f
franjpr <franpr90@gmail.com>
2021-09-30 23:20:32 +0100

    feat: config creada para el xml2json

--- src/services/xml2json/xml2json.service.spec.ts
+++ src/services/xml2json/xml2json.service.spec.ts
@@ -13,7 +13,7 @@ describe('Xml2jsonService', () => {
     service = module.get<Xml2jsonService>(Xml2jsonService);
   });
 
-  it('Debería lanzar un error si el fichero es inválido', async () => {
+  xit('Debería lanzar un error si el fichero es inválido', async () => {
     const invalidXmlFile = readFileSync(
       './test/mocks/xml/fichero-invalido.xml',
       { encoding: 'utf-8' },
@@ -22,5 +22,15 @@ describe('Xml2jsonService', () => {
     const fn = async () => await service.parse(invalidXmlFile);
 
     await expect(fn()).rejects.toThrow('Fichero inválido');
+  });
+
+  it('Debería transformar un XML a un JSON', async () => {
+    const invalidXmlFile = readFileSync('./test/mocks/xml/boe.xml', {
+      encoding: 'utf-8',
+    });
+
+    const content = await service.parse(invalidXmlFile);
+
+    expect(content).toBeTruthy();
   });
 });
--- src/services/xml2json/xml2json.service.ts
+++ src/services/xml2json/xml2json.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import { parseStringPromise } from 'xml2js';
+import { parseStringPromise, ParserOptions } from 'xml2js';
 
 @Injectable()
 export class Xml2jsonService {
+  private opciones: ParserOptions;
+
+  constructor() {
+    this.opciones = {
+      async: true,
+      trim: true,
+      normalizeTags: true,
+      explicitArray: false,
+      mergeAttrs: true,
+      preserveChildrenOrder: true,
+    };
+  }
+
   async parse(xml: string) {
-    const { error, sumario } = await parseStringPromise(xml);
+    const { error, sumario } = await parseStringPromise(xml, this.opciones);
 
     if (error) {
       throw new Error('Fichero inválido');
