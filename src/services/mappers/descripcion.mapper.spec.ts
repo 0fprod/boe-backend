f7f2f6c048b314e254c6ef0ae82c399d9189446d
franjpr <franpr90@gmail.com>
2021-10-05 22:41:26 +0100

    feat: añadido mappers para la descripcion

---
+++ src/services/mappers/descripcion.mapper.spec.ts
@@ -0,0 +1,15 @@
+import { ListaDeDefinicion } from '@services/models';
+import { mockDeBeneficiarioAnidado } from '@test/mocks/ts/beneficiario-anidado';
+import { descripcionMapper } from './descripcion.mapper';
+
+describe('Descripcion Specs', () => {
+  it.each([undefined, null, '', {}, []])('Devuelve cadena vacía si los argumentos son inválidos [%s]', (parametroInvalido) => {
+    expect(descripcionMapper(parametroInvalido as ListaDeDefinicion)).toEqual('');
+  });
+
+  it('Devuelve la descripcion principal de un documento', () => {
+    expect(descripcionMapper(mockDeBeneficiarioAnidado)).toEqual(
+      'Selección de empresas para el suministro de material de ferretería, eléctrico, construcción, fontanería y repuestos de automoción a las BAE,S ubicadas dentro del ámbito de la JIAE ESTE.',
+    );
+  });
+});
