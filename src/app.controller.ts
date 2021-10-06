88eeccaa5dee7f6d263dd9c6b2ded01b22117997
franjpr <franpr90@gmail.com>
2021-10-06 15:25:16 +0100

    config: agregado las variables d entorno

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private configService: ConfigService) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.configService.get('DATABASE_USER');
+    // return this.appService.getHello();
   }
 }
