3c4383e7e68434085cf9bdd2805f94dbceffc0af
franjpr <franpr90@gmail.com>
2021-10-20 17:56:37 +0100

    chore: añadir mocks sin lotes

---
+++ test/mocks/ts/beneficiario-sin-lote-mapeado.ts
@@ -0,0 +1,13 @@
+import { Beneficiario } from '@compartido/models';
+
+export const MockBeneficiariosSinLoteMapeado: Beneficiario[] = [
+  {
+    nif: 'B87140331.',
+    nombre: 'Gestión de Archivos y Documentación, S.L. (GesArDoc).',
+    coste: 14876.03,
+    esPyme: true,
+    lote: '',
+    descripcion:
+      'Catalogación de series cartográficas del siglo xx, cartografía de España siglo xx en gran formato y cartas náuticas (CPV: 79995200).',
+  },
+];
---
+++ test/mocks/ts/beneficiarios-sin-lote.ts
@@ -0,0 +1,20 @@
+import { ListaDeDefinicion } from '@compartido/api-models';
+
+export const MockBeneficiariosSinLote: ListaDeDefinicion = {
+  dt: ['3. Descripción de la licitación:', '5. Adjudicatarios:', '6. Valor de las ofertas:'],
+  dd: [
+    'Catalogación de series cartográficas del siglo xx, cartografía de España siglo xx en gran formato y cartas náuticas (CPV: 79995200).',
+    {
+      dl: {
+        dt: ['5.1) Nombre:', '5.2) Número de identificación fiscal:', '5.7) País:', '5.13)'],
+        dd: ['Gestión de Archivos y Documentación, S.L. (GesArDoc).', 'B87140331.', 'España.', 'El adjudicatario es una PYME.'],
+      },
+    },
+    {
+      dl: {
+        dt: '6.1) Valor de la oferta seleccionada:',
+        dd: '14.876,03 euros.',
+      },
+    },
+  ],
+};
