27ec8539e8f9adc9b8605d473800e567915512cc
franjpr <franpr90@gmail.com>
2021-10-20 18:01:39 +0100

    feat: implementado las llamadas al boe api

--- src/boe/boe.controller.ts
+++ src/boe/boe.controller.ts
@@ -1,25 +1,40 @@
-import { Controller, Get } from '@nestjs/common';
+import { Anuncio, Sumario } from '@compartido/api-models';
+import { Boe, Contrato } from '@compartido/models';
+import { Controller, Get, Param, Post } from '@nestjs/common';
+import { firstValueFrom } from 'rxjs';
 import { BoeService } from './boe.service';
 
 @Controller('boe')
 export class BoeController {
   constructor(private boeService: BoeService) {}
 
-  @Get()
+  @Get('/sumario/:id')
+  public async sumario(@Param() params): Promise<Sumario> {
+    const { id } = params;
+    return firstValueFrom(this.boeService.obtenerSumario(id));
+  }
+
+  @Get(':id')
+  public async boe(@Param() params): Promise<Boe> {
+    const { id } = params;
+    return firstValueFrom(this.boeService.obtenerBoe(id));
+  }
+
+  @Get('/anuncio/:id')
+  public anuncioNativo(@Param() params): Promise<Anuncio> {
+    const { id } = params;
+    return firstValueFrom(this.boeService.obtenerAnuncio(id));
+  }
+
+  @Get('/contrato/:id')
+  public anuncioParceado(@Param() params): Promise<Contrato> {
+    const { id } = params;
+    return firstValueFrom(this.boeService.obtenerContrato(id));
+  }
+
+  @Post('/cron')
   public anunciosId(boeId: string): string {
     // Es atacado por un CRONJOB para obtener los anuncios del día a través del boe
-    return this.boeService.getHello();
-  }
-
-  @Get()
-  public anuncioNativo(idAnuncio: string): string {
-    // Llamamos a la API del boe y devolvemos JSON sin parsear
-    return this.boeService.getHello();
-  }
-
-  @Get()
-  public anuncioParceado(idAnuncio: string): string {
-    // Llamamos a la API del boe y devolvemos JSON parseado
     return this.boeService.getHello();
   }
 }
