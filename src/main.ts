0b5c2dd745d1a3912d27736318fac4827b7d03c4
franjpr <franpr90@gmail.com>
2021-10-21 11:16:27 +0100

    test: prueba con path module-alias

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { addAliases } from 'module-alias';
+import { resolve } from 'path';
 import { AppModule } from './app.module';
+
+addAliases({
+  '@boe': resolve(__dirname, 'boe'),
+  '@contratos': resolve(__dirname, 'contratos'),
+  '@compartido': resolve(__dirname, 'compartido'),
+});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
--- tsconfig-paths-bootstrap.js
+++
@@ -1,7 +0,0 @@
-import Paths from 'tsconfig-paths';
-import TsConfig from './tsconfig.json';
-const baseUrl = './dist';
-Paths.register({
-  baseUrl,
-  paths: TsConfig.compilerOptions.paths,
-});
