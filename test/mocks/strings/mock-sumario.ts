b3bfed5fb4f0c10b939f05c7b1969489f73ffc6b
franjpr <franpr90@gmail.com>
2021-10-23 12:13:01 +0100

    chore: mocks de xml en string

---
+++ test/mocks/strings/mock-sumario.ts
@@ -0,0 +1,2 @@
+export const SumarioIrrelevante =
+  '<?xml version="1.0" encoding="UTF-8"?><sumario><meta></meta><diario><seccion num="5A"><departamento><item id="identificador irrelevante-1"><titulo>Anuncio de formalización de contratos. Objeto: Titulo irrelvante. Expediente:.</titulo></item><item id="identificador irrelevante-2"><titulo>Anuncio de formalización de contratos. Objeto: Titulo irrelvante2. Expediente:.</titulo></item></departamento></seccion></diario></sumario>';
