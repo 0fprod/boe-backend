89950b2e7cfa0be7f6c72e7a2e58a53cca7c5f74
franjpr <franpr90@gmail.com>
2021-10-25 16:29:02 +0100

    Feat/config db connection (#13)
    * refactor: renombrar fichero repository
    
    * fix: renombrar propiedad en el schema
    
    * feat: inyectar repo en el servicio del boe
    
    * feat: crear endpoint de prueba y conexion a mongodb
    
    * fix: arreglar el mapper de costes para cuando vienen cifras de millones
    
    * refactor: creado mock del repo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoeModule } from './boe/boe.module';
 import { ContratosModule } from './contratos/contratos.module';
-
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.env.local', '.env.dev', '.env'],
       isGlobal: true,
     }),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
     ContratosModule,
     BoeModule,
   ],
--- src/compartido/compartido.module.ts
+++ src/compartido/compartido.module.ts
@@ -3,12 +3,13 @@ import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BoeApiService } from './boe-api.service';
+import { ContratoRepository } from './contrato.repository';
 import { ContratoEntity, ContratoSchema } from './schema/contrato.schema';
 import { Xml2jsonService } from './xml2json.service';
 
 @Module({
   imports: [HttpModule, MongooseModule.forFeature([{ name: ContratoEntity.name, schema: ContratoSchema }])],
-  providers: [BoeApiService, ConfigService, Xml2jsonService],
-  exports: [HttpModule, BoeApiService, ConfigService, Xml2jsonService],
+  providers: [BoeApiService, ConfigService, Xml2jsonService, ContratoRepository],
+  exports: [HttpModule, BoeApiService, ConfigService, Xml2jsonService, ContratoRepository],
 })
 export class CompartidoModule {}
