9f5e771bc2610fd94756a9efa5fec49887018f21
franjpr <franpr90@gmail.com>
2021-10-28 11:29:24 +0100

    feat: implementados los tests del servicio de contratos por fechas

--- src/contratos/contratos.service.spec.ts
+++ src/contratos/contratos.service.spec.ts
@@ -1,26 +1,41 @@
+import { getModelToken, MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
-import { MockRepository } from '../../test/mocks/services/repository.mock';
+import { Model } from 'mongoose';
+import { closeInMongodConnection, rootMongooseTestModule } from '../../test/mongo.memory.server';
 import { ContratoRepository } from '../compartido/contrato.repository';
-import { construirContrato } from '../compartido/models';
+import { construirContrato, Contrato } from '../compartido/models';
+import { ContratoDoc, ContratoEntity, ContratoSchema } from '../compartido/schema/contrato.schema';
 import { ContratosService } from './contratos.service';
+
+const buscarPorId = (coleccion: Contrato[], idContrato: string): number => coleccion.findIndex((c) => c.contratoId === idContrato);
 
 describe('ContratosService', () => {
   let servicio: ContratosService;
-  let repositorio: ContratoRepository;
+  let model: Model<ContratoDoc>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        ContratosService,
-        {
-          provide: ContratoRepository,
-          useClass: MockRepository,
-        },
-      ],
+      imports: [rootMongooseTestModule(), MongooseModule.forFeature([{ name: ContratoEntity.name, schema: ContratoSchema }])],
+      providers: [ContratosService, ContratoRepository],
     }).compile();
 
     servicio = module.get<ContratosService>(ContratosService);
-    repositorio = module.get<ContratoRepository>(ContratoRepository);
+    model = module.get<Model<ContratoDoc>>(getModelToken(ContratoEntity.name));
+
+    await model.insertMany([
+      construirContrato({ contratoId: 'contratoId-1', fechaPub: new Date(2020, 5, 1).toISOString() }),
+      construirContrato({ contratoId: 'contratoId-2', fechaPub: new Date(2020, 5, 3).toISOString() }),
+      construirContrato({ contratoId: 'contratoId-3', fechaPub: new Date(2020, 5, 5).toISOString() }),
+      construirContrato({ contratoId: 'contratoId-4', fechaPub: new Date(2020, 5, 5).toISOString() }),
+    ]);
+  });
+
+  afterEach(async () => {
+    await model.deleteMany({});
+  });
+
+  afterAll(async () => {
+    await closeInMongodConnection();
   });
 
   it('Se monta el servicio', () => {
@@ -28,9 +43,27 @@ describe('ContratosService', () => {
   });
 
   it('Devuelve un contrato dado un id de contrato', async () => {
-    jest.spyOn(repositorio, 'obtenerContratoPorId').mockReturnValue(Promise.resolve(construirContrato({ contratoId: 'contratoId' })));
-    const contrato = await servicio.obtenerContratoPorId('contratoId');
+    const contrato = await servicio.obtenerContratoPorId('contratoId-1');
     expect(contrato).toBeTruthy();
-    expect(contrato.contratoId).toEqual('contratoId');
+    expect(contrato.contratoId).toEqual('contratoId-1');
+  });
+
+  it('Devuelve los contratos de un día dada una fecha', async () => {
+    const fechaPub = new Date(2020, 5, 5).toISOString();
+    const contratos = await servicio.obtenerContratoPorFecha(fechaPub);
+    expect(contratos).toBeTruthy();
+    expect(contratos).toHaveLength(2);
+    expect(buscarPorId(contratos, 'contratoId-3')).toBeGreaterThan(-1);
+    expect(buscarPorId(contratos, 'contratoId-4')).toBeGreaterThan(-1);
+  });
+
+  it('Devuelve los contratos que esten en un rango de fecha dado', async () => {
+    const inicio = new Date(2020, 5, 1).toISOString();
+    const fin = new Date(2020, 5, 3).toISOString();
+    const contratos = await servicio.obtenerContratoPorRangoFecha(inicio, fin);
+    expect(contratos).toBeTruthy();
+    expect(contratos).toHaveLength(2);
+    expect(buscarPorId(contratos, 'contratoId-1')).toBeGreaterThan(-1);
+    expect(buscarPorId(contratos, 'contratoId-2')).toBeGreaterThan(-1);
   });
 });
--- src/contratos/contratos.service.ts
+++ src/contratos/contratos.service.ts
@@ -9,4 +9,12 @@ export class ContratosService {
   obtenerContratoPorId(idContrato: string): Promise<Contrato> {
     return this.repositorio.obtenerContratoPorId(idContrato);
   }
+
+  obtenerContratoPorFecha(fechaPub: string): Promise<Contrato[]> {
+    return this.repositorio.obtenerContratoPorFecha(fechaPub);
+  }
+
+  obtenerContratoPorRangoFecha(fechaPubInicio: string, fechaPubFin: string): Promise<Contrato[]> {
+    return this.repositorio.obtenerContratoPorRangoDeFecha(fechaPubInicio, fechaPubFin);
+  }
 }
