b36b00a6563568a6959ba0937917682b6988dc31
franjpr <franpr90@gmail.com>
2021-11-03 16:47:38 +0000

    fix: implementado los tests del boe service

--- src/boe/boe.service.spec.ts
+++ src/boe/boe.service.spec.ts
@@ -1,27 +1,66 @@
+import { HttpModule } from '@nestjs/axios';
+import { getModelToken, MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Model } from 'mongoose';
+import { of } from 'rxjs';
+import { mockBoeApiService } from '../../test/mocks/services/boeapi.service.mock';
+import { rootMongooseTestModule } from '../../test/mongo.memory.server';
 import { BoeApiService } from '../compartido/boe-api.service';
 import { ContratoRepository } from '../compartido/contrato.repository';
+import { construirBoe, construirContrato } from '../compartido/models';
+import { ContratoDoc, ContratoEntity, ContratoSchema } from '../compartido/schema/contrato.schema';
 import { BoeService } from './boe.service';
 
 describe('BoeService', () => {
   let service: BoeService;
+  let api: BoeApiService;
+  let repo: Model<ContratoDoc>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BoeService, BoeApiService, ContratoRepository],
+      imports: [HttpModule, rootMongooseTestModule(), MongooseModule.forFeature([{ name: ContratoEntity.name, schema: ContratoSchema }])],
+      providers: [
+        BoeService,
+        {
+          provide: BoeApiService,
+          useClass: mockBoeApiService,
+        },
+        ContratoRepository,
+      ],
     }).compile();
 
     service = module.get<BoeService>(BoeService);
+    api = module.get<BoeApiService>(BoeApiService);
+    repo = module.get<Model<ContratoDoc>>(getModelToken(ContratoEntity.name));
   });
 
   it('Se monta el servicio', () => {
     expect(service).toBeDefined();
   });
 
-  it('Se ejecuta el flow', () => {
-    // Mockear la respuesta de la api para que nos de el BOE con id de contratos
-    // Mockear la respuesta de la api para que nos de contratos
+  it('Se guardan en base de datos los contratos del día', async () => {
+    jest.spyOn(api, 'obtenerBoe').mockReturnValue(of(construirBoe(['id-irrelevante-1', 'id-irrelevante-2'])));
+    jest.spyOn(api, 'obtenerContrato').mockReturnValueOnce(
+      of(
+        construirContrato({
+          contratoId: 'id-irrelevante-1',
+          titulo: 'contrato-irrelevante',
+        }),
+      ),
+    );
+    jest.spyOn(api, 'obtenerContrato').mockReturnValue(
+      of(
+        construirContrato({
+          contratoId: 'id-irrelevante-2',
+          titulo: 'contrato-irrelevante',
+        }),
+      ),
+    );
 
-    expect(service).toBeDefined();
+    const totalContratosGuardados = await service.guardarContratosPorBoeId('id-irrelevante');
+    const totalContratosEnBDD = await repo.find({});
+
+    expect(totalContratosGuardados).toEqual(2);
+    expect(totalContratosEnBDD).toHaveLength(2);
   });
 });
