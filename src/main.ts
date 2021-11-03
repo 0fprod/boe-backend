5c72a569fd776d8b6fa7e204aa60fac11dc4c10c
franjpr <franpr90@gmail.com>
2021-11-03 22:09:15 +0000

    feat: configurar swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Contratos públicos formalizados')
+    .setDescription('Esta API sirve datos de contratos formalizados anunciados en la api del BOE en la sección de 5A de anuncios.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
