e1dba061b62ee703f0e9de8329d0071d0d317078
franjpr <franpr90@gmail.com>
2021-11-04 21:44:16 +0000

    test: css inline

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { swaggerUi } from './swagger-styles';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('', app, document, {
     customSiteTitle: 'Formalización de contratos del BOE',
+    customCss: `.download-url-wrapper {display:none !important;} ${swaggerUi}`,
   });
 
   await app.listen(3000);
