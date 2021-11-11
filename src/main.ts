3bb98527b76bbbe056f9d04d30cc97512032e9b1
franjpr <franpr90@gmail.com>
2021-11-11 19:26:52 +0000

    fix: corst

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['https://boe-frontend.vercel.app/'],
+    origin: 'https://boe-frontend.vercel.app',
     methods: 'GET',
   });
 
