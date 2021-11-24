99bb745df38748da12f8ccf33adb64bdbdb53aca
franjpr <franpr90@gmail.com>
2021-11-24 16:01:38 +0000

    refactor: modelado de api para las estadisticas y consultas basiscas

--- src/estadisticas/estadisticas.repository.ts
+++ src/estadisticas/estadisticas.repository.ts
@@ -2,12 +2,78 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { ContratoDoc, ContratoEntity } from '../compartido/schema/contrato.schema';
+import { EstadisticaApiModel } from './models/api.model';
 
 @Injectable()
 export class EstadisticasRepository {
   constructor(@InjectModel(ContratoEntity.name) private contratoModel: Model<ContratoDoc>) {}
 
-  async obtenerTopBeneficiariosPorFecha(fechaPubInicio: string, fechaPubFin: string): Promise<any[]> {
+  async estadisticasPyme(fechaPubInicio: string, fechaPubFin: string): Promise<EstadisticaApiModel[]> {
+    const contratoDoc = await this.contratoModel.aggregate([
+      {
+        // Filtramos por rango de fecha
+        $match: {
+          fechaPub: {
+            $gte: fechaPubInicio,
+            $lte: fechaPubFin,
+          },
+        },
+      },
+      {
+        // Extraemos un document por cada beneficiario
+        $unwind: {
+          path: '$detalles.beneficiarios',
+          preserveNullAndEmptyArrays: false,
+        },
+      },
+      {
+        // Agrupamos por PYME/NoPyme y total de gastos
+        $group: {
+          _id: '$detalles.beneficiarios.esPyme',
+          total: { $sum: '$detalles.beneficiarios.coste' },
+        },
+      },
+    ]);
+
+    return contratoDoc;
+  }
+
+  async estadisticasPorActividad(fechaPubInicio: string, fechaPubFin: string): Promise<EstadisticaApiModel[]> {
+    const contratoDoc = await this.contratoModel.aggregate([
+      {
+        // Filtramos por rango de fecha
+        $match: {
+          fechaPub: {
+            $gte: fechaPubInicio,
+            $lte: fechaPubFin,
+          },
+        },
+      },
+      {
+        // Extraemos un document por cada beneficiario
+        $unwind: {
+          path: '$detalles.beneficiarios',
+          preserveNullAndEmptyArrays: false,
+        },
+      },
+      {
+        // Agrupamos por actividad de la institución y total de gastos
+        $group: {
+          _id: '$detalles.institucion.actividad',
+          total: { $sum: '$detalles.beneficiarios.coste' },
+        },
+      },
+      {
+        $sort: {
+          _id: 1,
+        },
+      },
+    ]);
+
+    return contratoDoc;
+  }
+
+  async obtenerTopBeneficiariosPorFecha(fechaPubInicio: string, fechaPubFin: string): Promise<EstadisticaApiModel[]> {
     const contratoDoc = await this.contratoModel.aggregate([
       {
         // Filtramos por rango de fecha
@@ -29,19 +95,19 @@ export class EstadisticasRepository {
         // Agrupamos por nombre y numero de apariciones
         $group: {
           _id: '$detalles.beneficiarios.nombre',
-          numVeces: { $sum: 1 },
+          total: { $sum: 1 },
         },
       },
       {
         // Filtramos las que aparezca más de una vez
         $match: {
-          numVeces: { $gt: 1 },
+          total: { $gt: 1 },
         },
       },
       {
         // Ordenamos de mayor a menor
         $sort: {
-          numVeces: -1,
+          total: -1,
         },
       },
       {
--- src/estadisticas/models/estadisticas.model.ts
+++ src/estadisticas/models/estadisticas.model.ts
@@ -1,9 +1,21 @@
+import { EstadisticaApiModel } from './api.model';
+
 export interface Estadistica {
   etiqueta: string;
   valor: number;
 }
 
-export const mapNumContratosPorBeneficiario = (doc: any): Estadistica => ({
+export const mapNumContratosPorBeneficiario = (doc: EstadisticaApiModel): Estadistica => ({
   etiqueta: doc._id,
-  valor: doc.numVeces,
+  valor: doc.total,
+});
+
+export const mapEstadisticaPYMES = (doc: EstadisticaApiModel): Estadistica => ({
+  etiqueta: doc._id ? 'PYME' : 'No PYME',
+  valor: doc.total,
+});
+
+export const mapEstadisticaActividad = (doc: EstadisticaApiModel): Estadistica => ({
+  etiqueta: doc._id ? doc._id : 'Sin actividad.',
+  valor: doc.total,
 });
