76d59ab68c2e9fcef0c8099cfe6a3fa98e4a7b14
franjpr <franpr90@gmail.com>
2022-01-26 17:31:05 +0000

    feat: instalar helmet

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { swaggerUi } from './swagger-styles';
 
@@ -11,12 +12,16 @@ async function bootstrap() {
     methods: 'GET',
   });
 
+  app.use(helmet());
+
   const config = new DocumentBuilder()
     .setTitle('Contratos públicos formalizados')
     .setDescription('Esta API sirve datos de contratos formalizados anunciados en la api del BOE en la sección de 5A de anuncios.')
     .setVersion('1.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
+
   SwaggerModule.setup('', app, document, {
     customSiteTitle: 'Formalización de contratos del BOE',
     customCss: `.download-url-wrapper {display:none !important;} ${swaggerUi}`,
